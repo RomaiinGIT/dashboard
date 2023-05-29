95eb9706fba57ae6b2bc7b1e1ceb0bd38d72d39e
Romain Lejeune <53431778+RomaiinGIT@users.noreply.github.com>
2023-05-29 22:43:43 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -4,6 +4,23 @@
     $result = file_get_contents($url);
     $result = json_decode($result, true);
     $config = $result["config"];
+
+    if (isset($_POST['modify'])) {
+
+        $firstparam = $_POST['firstparam'] ?? "0";
+        $secondparam = $_POST['secondparam'] ?? "0";
+        $value = $_POST['value'];  
+    
+        echo $firstparam;
+        echo $secondparam;
+        echo $value;
+    
+        $url = "http://127.0.0.1:8080/write/" . rawurlencode($firstparam) . "/" . rawurlencode($secondparam) . "/" . rawurlencode($value);
+        $result = file_get_contents($url);
+        $result = json_decode($result, true);
+        print_r($result);
+    };
+
 ?>
 
 <!DOCTYPE html>
@@ -25,22 +42,21 @@
 
 foreach($config as $key => $value){
     if ( ! is_array($value)){
-        echo "<h3 class='titles'>.", $key, "<div class='inputgrp'><input type='text' value='", $value, "'><button class='btn' type='button'>Modifier</button></div></h3>";
+        echo "<h3 class='titles'>.", $key, "<div class='inputgrp'><form action='' method='post'><input type='text' name='value' value='", $value, "'><button class='btn' name='modify' type='input'>Modifier</button><input name='firstparam' value='", $key, "' type='hidden'></form></div></h3>";
     } else {
         echo "<h2 class='titleh'>.", $key, "</h2>";
         foreach ($value as $keyv => $valuev) {
             if ( ! is_array($valuev)){
-                echo "<h3 class='titles'>.", $keyv, "<div class='inputgrp'><input type='text' value='", $valuev, "'><button class='btn' type='button'>Modifier</button></div></h3>";
+                echo "<h3 class='titles'>.", $keyv, "<div class='inputgrp'><form action='' method='post'><input name='value' type='text' value='", $valuev, "'><button class='btn' name='modify' type='input'>Modifier</button><input name='firstparam' value='", $key, "' type='hidden'><input name='secondparam' value='", $keyv, " ' type='hidden'></form></div></h3>";
             } else {
                 echo "<h2 class='titleh'>.", $keyv, "</h2>"; 
                 foreach ($valuev as $keyvv => $valuevv) {
-                    echo "<h3 class='titles'>.", $keyvv, " <div class='inputgrp'><input type='text' value='", $valuevv, "'><button class='btn' type='button'>Modifier</button></div></h3>";
+                    echo "<h3 class='titles'>.", $keyvv, " <div class='inputgrp'><form action='' method='post'><input name='value' type='text' value='", $valuevv, "'><button name='modify' type='input' class='btn'>Modifier</button><input name='firstparam' value='", $key, "' type='hidden'><input name='secondparam' value='", $keyvv, " ' type='hidden'></form></div></h3>";
                 }
             }
         }
     }
 };
-
 ?>
 
 </div>
